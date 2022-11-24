37d1a61de3495c4313ad3beeeceb295997776286
Oracle0Zero <112552488+Oracle0Zero@users.noreply.github.com>
2022-11-24 21:02:03 +0530

    Update App.java

--- maven_first_example/my-app/src/main/java/com/mycompany/app/App.java
+++ maven_first_example/my-app/src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         int a = 10;
-        System.out.println( "Hello Worl from DevOps!" );
+        System.out.println( "Hello Worl fom DevOps!" );
     }
 }
